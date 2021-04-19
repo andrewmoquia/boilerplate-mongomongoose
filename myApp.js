34d5fe77e9376e68c5931f6f90c040c728d984c7
andrewmoquia <andrewmoquia09@gmail.com>
2021-04-19 20:54:57 +0800

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 //step1: add mongoose and mongodb to the project
 const mongoose = require('mongoose')
-const mongodb = require('mongodb')
 
 //Step2: setup mongo atlas and connect to the database using the following syntax:
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
