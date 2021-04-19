2e26d66ae232d07d4f5d5bf3373232cefcc32f22
andrewmoquia <andrewmoquia09@gmail.com>
2021-04-19 20:44:40 +0800

    setup mongo atlas and connect to the database using the following syntax

--- myApp.js
+++ myApp.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 
+//setup mongo atlas and connect to the database using the following syntax:
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 let Person;
 
 const createAndSavePerson = (done) => {
